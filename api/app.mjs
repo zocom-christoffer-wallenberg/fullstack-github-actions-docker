06c699c07ca0476002ff65a03f5b9ae72e281ff5
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2025-10-22 14:11:55 +0200

    Fixed bug

--- api/app.mjs
+++ api/app.mjs
@@ -6,11 +6,14 @@ import "dotenv/config";
 
 import todoRouter from "./routes/todo.mjs";
 
+import { initDb } from "./service/db.mjs";
+
 app.use(json());
 app.use(cors());
 
 app.use("/api/todo", todoRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await initDb();
   console.log("Server started");
 });
